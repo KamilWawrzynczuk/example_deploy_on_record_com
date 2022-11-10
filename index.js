a88b0d1fad80ba278f4d661825553f33e6bdc62b
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2022-11-10 13:58:26 +0100

    Add 404

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
@@ -22,9 +23,12 @@ app.get("/multiply/:number", (req, res) => {
   const number = Number(req.params.number);
   res.send(`${number * process.env.MULTIPLIER}`);
 });
-app.get("*", function (req, res) {
-  res.status(404).send("Route Unknown");
-});
+
+// Handling non matching request from the client
+app.use((req, res, next) => {
+  res.status(404).send(
+      "<p>Route Unknown</p>")
+})
 
 app.listen(PORT, () => {
   console.log("Server is running at http://localhost:" + PORT);
